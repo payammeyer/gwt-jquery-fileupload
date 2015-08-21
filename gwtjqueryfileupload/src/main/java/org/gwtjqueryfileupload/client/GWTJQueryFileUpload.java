de404cd1a0b52298feaafc8129fa43748067e4fc
Payam Meyer <payam.meyer@gmail.com>
2015-08-21 11:01:29 -0400

    Initial setup

---
+++ gwtjqueryfileupload/src/main/java/org/gwtjqueryfileupload/client/GWTJQueryFileUpload.java
@@ -0,0 +1,17 @@
+package org.gwtjqueryfileupload.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.SimplePanel;
+
+
+public class GWTJQueryFileUpload implements EntryPoint {
+
+	private SimplePanel simplePanel;
+
+	@Override
+	public void onModuleLoad() {
+
+
+	}
+
+}
